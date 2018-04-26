e1c1697e867829bdbeea3ba51c7dbadf517438a1
tommytracey <tommytracey@gmail.com>
2018-04-26 15:26:26 -0500

    minor tweaks

--- p12-semantic-segmentation/main.py
+++ p12-semantic-segmentation/main.py
@@ -248,8 +248,8 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # Variable placeholders
-        correct_label = tf.placeholder(tf.int32, [None, None, None, NUM_CLASSES], name='correct_label')
-        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+        correct_label = tf.placeholder(tf.int32, [None, IMAGE_SHAPE[0], IMAGE_SHAPE[1], NUM_CLASSES], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, [], name='learning_rate')
 
         # Build NN using load_vgg, layers, and optimize function
         input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)

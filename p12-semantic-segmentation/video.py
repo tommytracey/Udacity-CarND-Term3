2d385886eb98ac20a2c3e5fd6c52a3f37fe33719
tommytracey <tommytracey@gmail.com>
2018-04-27 17:43:08 -0500

    fixes for video script

--- p12-semantic-segmentation/video.py
+++ p12-semantic-segmentation/video.py
@@ -3,5 +3,5 @@ import moviepy.editor as mpy
 # Create video from image predictions
 print('Creating video clip...')
 clip = mpy.ImageSequenceClip('runs/1524868008.9052386', fps=15)
-clip.write_videofile("video/video_out.mp4",fps=15, audio=False)
+clip.write_videofile("video_out.mp4",fps=15, audio=False)
 print('Video clip completed.')
